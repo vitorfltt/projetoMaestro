a54674e4cee1206f314ea8bda7438dd14874674c
Vitor Filetto <vitorfltt@hotmail.com>
2024-06-26 15:13:26 -0300

    all parts

--- join_parts.py
+++ join_parts.py
@@ -7,7 +7,9 @@ def join_files(output_file, part_files):
                 outfile.write(infile.read())
 
 if __name__ == "__main__":
-    part_files = ["part_aa", "part_ab", "part_ac"]  # Adicione todas as partes aqui na ordem correta
+    part_files = ["part_aa", "part_ab", "part_ac","part_ad","part_ae","part_af","part_ag","part_ah"]
     output_file = "app-casa_reconstructed.apk"
     join_files(output_file, part_files)
+    output_file_path = os.path.abspath(output_file)
     print(f"Arquivo reconstruído: {output_file}")
+    print(f"Caminho completo do arquivo: {output_file_path}")
